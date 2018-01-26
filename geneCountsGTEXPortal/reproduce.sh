f29e9437409b5d5b2e2fb100ce7268669d128591
Serghei Mangul <smangul@oda-compute-0-5.oda-internal.com>
2018-01-25 17:48:43 -0800

    select non-brain genes

--- geneCountsGTEXPortal/reproduce.sh
+++ geneCountsGTEXPortal/reproduce.sh
@@ -2,3 +2,5 @@ python ../code/select.brain.genes.py GTEx_Analysis_v6_RNA-seq_RNA-SeQCv1.1.8_gen
 
 #select genes present at least in 1250 samples
 awk -F "," '{if ($3>1250) print}' genes.GTEX.tsv | awk -F "." '{print $1}' >genes.GTEX.brain.2500.txt
+
+awk -F "," '{if ($3<=1250) print}' genes.GTEX.tsv | awk -F "." '{print $1}' >genes.GTEX.non-brain.txt
